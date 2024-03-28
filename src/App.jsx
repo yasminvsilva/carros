cf28430eef269196c4439673a7f2f0edfcef7dca
Yasmin <alunoyasminsilva07@gmail.com>
2024-03-27 23:24:36 -0300

    quinto commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import iconLuxury from "./assets/icon-luxury.svg"
 
 export default function App() {
   return (
-    <main>
+    <main className="min-h-svh bg-offwhitebg py-20 px-6">
       <section>
         <div className="bg-orange">
           <img src={iconSedans} alt="Ícone dos carros Sedan." />
