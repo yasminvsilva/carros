8471c287ea760485c5ec89b1c3d48d136e01c132
Yasmin <alunoyasminsilva07@gmail.com>
2024-03-27 23:06:04 -0300

    instalação do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'orange': '#E28625',
+      'green': '#006971',
+      'dark-green': '#004140',
+      'offwhite': 'rgba(255, 255, 255, 0.75)',
+      'white': '#FFF',
+      'offwhitebg': '#F2F2F2'
+    }
+  },
+  plugins: [],
+}
