20a857b4be1be6b78efb16ca89f28fbd31b6f14b
Yasmin <alunoyasminsilva07@gmail.com>
2024-03-28 22:59:35 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import Luxury from "./components/Luxury"
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
 
         <Sedans /> 
 
         <Suvs />
 
         <Luxury />
-      </section>
+      </div>
     </main>
   )
 }
